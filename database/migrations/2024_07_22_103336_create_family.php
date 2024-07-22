a60a530a8414fce53f1961c55bafaedbd85bbee7
lafathalfath <iqnaraidan12@gmail.com>
2024-07-22 19:21:29 +0700

    CREATE: seeder: family, jenis, kategori, entitas

--- database/migrations/2024_07_22_103336_create_family.php
+++ database/migrations/2024_07_22_103336_create_family.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('family', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('nama');
             $table->timestamps();
         });
     }
