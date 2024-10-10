08920d6e7508f9a21d447429166b00cae0ee9f43
husnimr <husniramadhan@apps.ipb.ac.id>
2024-10-10 17:28:47 +0700

    CREATE: EntitasDetailSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,6 +18,7 @@ class DatabaseSeeder extends Seeder
             JenisSeeder::class,
             kategoriSeeder::class,
             EntitasSeeder::class,
+            EntitasDetailSeeder::class,
             ProvinsiSeeder::class,
             WelcomeTextSeeder::class,
             UsiaSeeder::class,
